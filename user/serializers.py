b6a34c66748b1370fe20ac2f365cef592459bc42
Daniel Suniaga <danieldsuniaga@gmail.com>
2022-08-24 14:18:33 -0500

    gestion de usuarios

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+
+from django.utils.translation import ugettext_lazy as _
 
 from  rest_framework import serializers
 
@@ -13,10 +15,15 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email','password','name')
 
         extra_kwargs = {
+
             'password':{
+
                 'write_only':True, 
+
                 'min_length':5
+
             }
+            
         }
 
     def create(self, validated_data):
@@ -24,4 +31,65 @@ class UserSerializer(serializers.ModelSerializer):
          """ Crear un nuevo usuario con clave encriptada y retornarlo """
 
          return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+
+        """ Actualizar el usuario,configura el password correctamente y lo retorna """
+
+        password = validated_data.pop('password', None)
+
+        user = super().update(instance, validated_data)
+
+        if password:
+
+            user.set_password(password)
+
+            user.save()
+
+        return user
+
+class AuthTokenSerializer(serializers.Serializer):
+
+    """ Serializador para el objeto de autenticación del usuario """
+
+    email = serializers.CharField()
+
+    password = serializers.CharField(
+
+        style = {'input_type':'password'},
+
+        trim_whitespace = False
+
+    )
+
+    def validate(self, attrs):
+
+        """ Validar y autenticr usuario """
+
+        email = attrs.get('email')
+
+        password = attrs.get('password')
+
+        user = authenticate(
+
+            request = self.context.get('request'),
+
+            username = email,
+
+            password = password
+
+        )
+
+        if not user: 
+
+            msg =_('Unable to authenticate with provided credentials')
+
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        
+        return attrs
+
+
+
 
--- user/views.py
+++ user/views.py
@@ -1,9 +1,38 @@
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 
-from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from rest_framework import generics, authentication, permissions
+
+from rest_framework.settings import api_settings
 
 class CreateUserView(generics.CreateAPIView):
 
     """Crear nuevo usuario en el sistema"""
 
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+
+    """ Crear nuevo auth token para usuario """
+
+    serializer_class = AuthTokenSerializer
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveAPIView):
+
+    """ Manejar usuario autenticado """
+
+    serializer_class = UserSerializer
+
+    authentication_classes = (authentication.TokenAuthentication,)
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """ Obtener y retornar usuario autenticado """
+
+        return self.request.user
+
+
